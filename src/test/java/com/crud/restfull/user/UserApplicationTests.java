d05749a15ad73a6cbdf437f4e85697593e48e5d9
Frank Diaz <fdiaz@interbanking.com.ar>
2023-12-08 11:10:23 -0300

    first commit

---
+++ src/test/java/com/crud/restfull/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.restfull.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
